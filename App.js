85694377ce19723ae6f97893492170e503c1cb43
SonaamMourya04 <sonammourya7683@gmail.com>
2025-02-16 01:01:46 +0530

    commit

--- App.js
+++ App.js
@@ -2,9 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 
-const element = React.createElement("div", null, 
-    React.createElement("h1", null, "Welcome to EarthBites🚀"),
-    React.createElement("p", null, "This is a basic React app")
+const element = React.createElement(
+    "div",
+     null, 
+    React.createElement(
+        "h1",
+         null,
+          "Welcome to EarthBites🚀"),
+    React.createElement(
+        "p", 
+        null, 
+        "This is a basic React app")
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
